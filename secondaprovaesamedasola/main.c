3af3d36aef496518a20e4e443ed1024909b1e91b
Rosemarie Pirlo <rosemariepirlo@mbpdirosemarie.homenet.telecomitalia.it>
2019-01-04 08:01:21 +0100

    Initial Commit

---
+++ secondaprovaesamedasola/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  secondaprovaesamedasola
+//
+//  Created by Rosemarie Pirlo on 04/01/2019.
+//  Copyright © 2019 Rosemarie Pirlo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
